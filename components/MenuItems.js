d89e0e341ce2101fdee88f684827e623f325798b
jadashi97 <njadauniversal23@yahoo.com>
2022-12-16 06:39:29 -0600

    (feat):finished large lists with FlatList. (Next): complete SectionList

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -2,43 +2,49 @@ import React from 'react';
 import { ScrollView, View, Text, StyleSheet, FlatList} from 'react-native';
 
 const menuItemsToShow = [
-  {name: 'Keimot', id: '1A'},
-  {name: 'samag', id: '2B'},
-  {name: 'bamiya', id: '3C'},
-  {name: 'ades', id: '4D'},
-  {name: 'lubia', id: '5E'},
-  {name: 'basiko', id: '6F'},
-  {name: 'weeka', id: '7G'},
-  {name: 'gidad', id: '8H'},
-  {name: 'koruf', id: '9I'},
-  {name: 'ganamaya', id: '10J'},
-  {name: 'cabbage', id: '11K'},
-  {name: 'janjaru', id: '12L'}
+  {name: 'Keimot', price: '$3.00', id: '1A'},
+  {name: 'samag', price: '$5.00', id: '2B'},
+  {name: 'bamiya', price: '$5.00', id: '3C'},
+  {name: 'ades', price: '$4.00', id: '4D'},
+  {name: 'lubia', price: '$2.00', id: '5E'},
+  {name: 'basiko', price: '$5.00', id: '6F'},
+  {name: 'weeka', price: '$5.00', id: '7G'},
+  {name: 'gidad', price: '$7.00', id: '8H'},
+  {name: 'koruf', price: '$7.00', id: '9I'},
+  {name: 'ganamaya', price: '$7.00', id: '10J'},
+  {name: 'cabbage', price: '$5.00', id: '11K'},
+  {name: 'janjaru', price: '$3.00', id: '12B'},
+  {name: 'bambe', price: '$2.00', id: '1L'},
+  {name: 'bafura', price: '$2.00', id: '18C'},
+  {name: 'tamiya', price: '$2.00', id: 'DL'},
+  {name: 'sambusa', price: '$3.00', id: 'NL'},
 ];
 
-const Item = ({name}) => {
-  <view style={menuStyles.innerContainer}>
-    <Text style={menuStyles.itemText}>
-      {name}
-    </Text>
-  </view>
-}
+const Item = ({name, price}) => (
+  <View style={menuStyles.innerContainer}>
+      <Text style={menuStyles.itemText}>
+        {name}
+      </Text>
+      <Text style={menuStyles.itemText}>
+        {price}
+      </Text>
+  </View>
+);
 
 export default function MenuItems() {
-  const renderItem = ({ item }) => <Item name={item.name}/>;
+  const renderItem = ({ item }) => <Item name={item.name} price={item.price}/>;
 
   return (
     <View style={menuStyles.container}>
           <Text style={menuStyles.headerText}>
             View Menu
           </Text>
-          {/* <Text style={menuStyles.itemText}>
-              {menuItemsToShow[0]}
-          </Text> */}
-          <FlatList 
+          <FlatList
+            style={menuStyles.itemText}
             data={menuItemsToShow} 
             keyExtractor={(item) => item.id} 
-            renderItem={renderItem}/>
+            renderItem={renderItem}
+          ></FlatList>
     </View>
   )
 }
@@ -52,7 +58,9 @@ const menuStyles = StyleSheet.create({
   innerContainer: {
     paddingHorizontal: 40,
     paddingVertical: 20,
-    backgroundColor: 'black'
+    backgroundColor: 'black',
+    justifyContent: 'space-between',
+    flexDirection: 'row'
   },
   headerText:{
     color: 'white',
@@ -62,6 +70,6 @@ const menuStyles = StyleSheet.create({
   },
   itemText: {
     color: '#F4CE14',
-    fontSize: 36
+    fontSize: 30
   }
 })
