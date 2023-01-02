45c85f2742822911b4e769553fc4164019893e8b
jadashi97 <njadauniversal23@yahoo.com>
2023-01-02 16:29:19 -0600

    (feat): learned hooks and added to app functionality

--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,54 +1,75 @@
 import React from 'react';
-import { View, Text, Image, ScrollView, StyleSheet} from 'react-native';
+import { View, Text, Image, ScrollView, StyleSheet, useColorScheme, useWindowDimensions} from 'react-native';
 
 export default function WelcomeScreen() {
-  return (
-        <ScrollView
-            indicatorStyle='white'
-            style={styles.container}>
-            <View style={styles.headerWrapper}>
-                <Image style={styles.logo} source={require('../assets/myLogo.png')}/>
-                <Text style={styles.headerText}>
-                    Welcome to Natana Restaurant 
-                </Text> 
-            </View>
-            <Text
-                style={styles.mainInfo}>
-                The Natana Restaurant is a charming neighborhood bistro that 
-                serves simple food and classic cocktails in a casual environment.
-                We would like to see you more. We love that you are here to support us 
-                and offer back to the local community.
-            </Text>
-            <Image 
-                style={styles.image} 
-                source={require('../assets/pic1.png')}
-                resizeMode= "cover"
-                accessibility={true}
-                accessibilityLabel={'Natana Restaurant food'}
-            />
-            <Image 
-                style={styles.image} 
-                source={require('../assets/pic5.png')}
-                resizeMode= "cover"
-                accessibility={true}
-                accessibilityLabel={'Natana Restaurant food'}
-            />
-            <Image 
-                style={styles.image} 
-                source={require('../assets/pic3.png')}
-                resizeMode= "cover"
-                accessibility={true}
-                accessibilityLabel={'Natana Restaurant food'}
-            />
-            <Image 
-                style={styles.image} 
-                source={require('../assets/pic4.png')}
-                resizeMode= "cover"
-                accessibility={true}
-                accessibilityLabel={'Natana Restaurant food'}
-            />
-        </ScrollView>
-  )
+    const colorScheme = useColorScheme(); // used to switch between color themes
+    const window = useWindowDimensions(); // used to detail the app's dimensions
+
+    return (
+            <ScrollView
+                indicatorStyle='white'
+                style={[styles.container, 
+                        colorScheme === 'light'
+                        ? {backgroundColor: '#fff'}
+                        : {backgroundColor: '#333333'}
+                ]}>
+                <View style={styles.headerWrapper}>
+                    <Image style={styles.logo} source={require('../assets/myLogo.png')}/>
+                    <Text style={[styles.headerText,
+                        colorScheme === 'light'
+                        ? {color: '#333333'}
+                        : {color: '#EDEFEE'}
+                    ]}>
+                        Welcome to Natana Restaurant 
+                    </Text> 
+                </View>
+                <Text
+                    style={[
+                        styles.mainInfo,
+                        colorScheme === 'light'
+                        ? {color: '#333333'}
+                        : {color: '#EDEFEE'}
+                    ]}>
+                    
+                    The Natana Restaurant is a charming neighborhood bistro that 
+                    serves simple food and classic cocktails in a casual environment.
+                    We would like to see you more. We love that you are here to support us 
+                    and offer back to the local community.
+                </Text>
+                {/* <Image 
+                    style={styles.image} 
+                    source={require('../assets/pic1.png')}
+                    resizeMode= "cover"
+                    accessibility={true}
+                    accessibilityLabel={'Natana Restaurant food'}
+                />
+                <Image 
+                    style={styles.image} 
+                    source={require('../assets/pic5.png')}
+                    resizeMode= "cover"
+                    accessibility={true}
+                    accessibilityLabel={'Natana Restaurant food'}
+                />
+                <Image 
+                    style={styles.image} 
+                    source={require('../assets/pic3.png')}
+                    resizeMode= "cover"
+                    accessibility={true}
+                    accessibilityLabel={'Natana Restaurant food'}
+                />
+                <Image 
+                    style={styles.image} 
+                    source={require('../assets/pic4.png')}
+                    resizeMode= "cover"
+                    accessibility={true}
+                    accessibilityLabel={'Natana Restaurant food'}
+                /> */}
+                <Text style={styles.regular}>Window Dimensions</Text>
+                <Text style={styles.regular}>Height: {window.height}</Text>
+                <Text style={styles.regular}>Width: {window.width}</Text>
+                <Text style={styles.regular}>Font scale: {window.fontScale}</Text>
+            </ScrollView>
+    )
 };
 
 const styles = StyleSheet.create({
@@ -61,7 +82,7 @@ const styles = StyleSheet.create({
     logo:{
         height: 70,
         width: 70,
-        resizeMode: "cover",
+        resizeMode: "contain",
         borderRadius: 10,
     },
     headerText: {
