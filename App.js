8ede81df03a4d57e9e1f81a69f41f1ae43975652
jadashi97 <njadauniversal23@yahoo.com>
2023-01-05 16:43:48 -0600

    (feat): Learned to add bottom tabs to move btn screens back and forth

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Ionicons} from '@expo/vector-icons';
 import NatanaRestauHeader from './components/NatanaRestauHeader';
 import Footer from './components/Footer';
 import WelcomeScreen from './components/WelcomeScreen';
@@ -11,30 +13,57 @@ import LoginScreen from './components/LoginScreen';
 import FeedBackForm from './components/FeedBackForm';
 
 const Stack = createNativeStackNavigator(); //we are getting this from the native-stack
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <NatanaRestauHeader/>
-        <Stack.Navigator 
-          initialRouteName='Login'
-          screenOptions={{headerStyle: {backgroundColor: '#FBDABB'}}}  
-        >
-          <Stack.Screen name="Welcome" component={WelcomeScreen}/>
-          <Stack.Screen name="Login" component={LoginScreen}/>
-          <Stack.Screen name="Menu" component={MenuItems}/>
-          <Stack.Screen name="SectionList" component={RenderSectionList}/>
-        </Stack.Navigator>
-        {/* <MenuItems/> */}
-        {/* <RenderSectionList/> */}
-        {/* <FeedBackForm/> */}
+    ////// THIS IS FOR THE STACK NAVIGATOR //////////////
 
-      </View>
-      <View style={{backgroundColor: '#495E57'}}>
-        <Footer/>
-      </View>
+    // <NavigationContainer>
+    //   <View style={styles.container}>
+    //     <NatanaRestauHeader/>
+    //     <Stack.Navigator 
+    //       initialRouteName='Login'
+    //       screenOptions={{headerStyle: {backgroundColor: '#FBDABB'}}}  
+    //     >
+    //       <Stack.Screen name="Welcome" component={WelcomeScreen}/>
+    //       <Stack.Screen name="Login" component={LoginScreen}/>
+    //       <Stack.Screen name="Menu" component={MenuItems}/>
+    //       <Stack.Screen name="SectionList" component={RenderSectionList}/>
+    //     </Stack.Navigator>
+    //     {/* <MenuItems/> */}
+    //     {/* <RenderSectionList/> */}
+    //     {/* <FeedBackForm/> */}
+
+    //   </View>
+    //   <View style={{backgroundColor: '#495E57'}}>
+    //     <Footer/>
+    //   </View>
+    // </NavigationContainer>
+
+    ////// THIS IS FOR THE TAB NAVIGATOR //////////////
+    <NavigationContainer>
+      <Tab.Navigator
+        //below is to design & customize the icons
+        screenOptions={({route})=> ({
+          tabBarIcon: ({ focused, color, size}) => {
+            let iconName; 
+
+            if(route.name === 'Login') {
+              iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+            } else if ( route.name === 'Welcome'){
+              iconName = 'ios-list';
+            }
+            return <Ionicons nam={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name= "Login" component={LoginScreen}/>
+        <Tab.Screen name="Welcome" component={WelcomeScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
+
   );
 }
 
